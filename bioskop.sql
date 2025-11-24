c7aba95647127ec705f20887e213a3eb5a77c25d
sabrinanurhaliza <240411100204@student.trunojoyo.ac.id>
2025-11-24 20:22:31 +0700

    update sql

--- bioskop.sql
+++ bioskop.sql
@@ -22,6 +22,11 @@ CREATE TABLE studios (
     total_kursi_per_baris INT NOT NULL
 );
 
+INSERT INTO `studios` (`Id_studio`, `nama_studio`, `capacity`, `total_baris`, `total_kursi_per_baris`) VALUES
+(1, 'Regular 2D', 20, 4, 5),
+(2, 'Dolby Atmos', 0, 10, 120),
+(3, 'IMAX', 0, 10, 120);
+
 -- Tabel Movies
 CREATE TABLE movies (
     Id_movie INT PRIMARY KEY AUTO_INCREMENT,
@@ -31,6 +36,19 @@ CREATE TABLE movies (
     poster VARCHAR(255),
     release_date DATE
 );
+
+INSERT INTO `movies` (`Id_movie`, `judul`, `rating`, `genre`, `description`, `duration`, `director`, `poster`, `release_date`, `age_rating`) VALUES
+(1, 'Pangku', '8.7', 'Drama, Romance', 'Sartika, seorang perempuan muda yang hamil, meninggalkan kampung halamannya untuk mencari harapan di Pantura, dan bertemu Maya yang membantunya melalui kehidupan baru penuh tantangan dan cinta.', 120, 'Reza Rahadian', 'pangku.jpg', '2025-11-06', '17+'),
+(2, 'Kuncen', '7.9', 'Horror, Misteri', 'Awindya nekat mendaki Gunung Merapi untuk mencari Devlin yang hilang, namun kekuatan gelap pegunungan mulai mengancam jiwa mereka.', 105, 'Jose Poernomo', 'kuncen.jpg', '2025-11-06', '17+'),
+(3, 'Sosok Ketiga: Lintrik', '8.2', 'Horor, Drama', 'Andin merasa ada kekuatan gelap dalam rumah tangganya setelah enam bulan menikah; Naura menggunakan ilmu pelet “Lintrik” yang menghantui kehidupan Andin & Aryo.', 110, 'Fajar Nugros', 'sosok_ketiga_lintrik.jpg', '2025-11-06', '17+'),
+(4, 'Solata', '7.5', 'Drama, Religi', 'Angkasa menjadi relawan guru di Tana Toraja dan bertemu murid-murid yang mengubah pandangannya tentang kehidupan dan makna doa.', 110, 'Ody C. Harahap', 'solata.jpg', '2025-11-06', 'all'),
+(5, 'Dopamin', '9.0', 'Drama, Thriller', 'Malik dan Alya menghadapi krisis rumah tangga dan keuangan setelah Malik di-PHK; sebuah kejadian misterius terjadi saat seorang tamu asing datang dengan koper miliaran.', 95, 'Teddy Soeriaatmadja', 'dopamin.jpg', '2025-11-13', '13+'),
+(6, 'Wicked: For Good', '8.8', 'Musikal, Fantasi', 'Elphaba dan Glinda menghadapi pilihan moral dan ambisi dalam dunia sihir; konflik persahabatan dan kekuasaan mewarnai kisah mereka.', 137, 'Jon M.Chu', 'wicked_for_good.jpg', '2025-11-19', '13+'),
+(7, 'Now You See Me: Now You Don’t', '8.6', 'Thriller, Kejahatan', 'Para pesulap Four Horsemen dan generasi baru bersatu untuk mencuri berlian besar milik perusahaan kriminal dan membongkar konspirasi global.', 125, 'Ruben Fleischer', 'now_you_see_me_3.jpg', '2025-11-14', '13+'),
+(8, 'Avatar 3', '9.5', 'action-adventure, science fiction, and fantasy', 'The next chapter of the Avatar saga.', 190, 'James Cameron', 'avatar3.jpg', '2025-12-20', '13+'),
+(12, 'Five Nights at Freddy’s 2', '0', 'Horror', 'Sekuel menegangkan dari franchise animatronik horror. ‒ akan tayang Desember 2025 ', 100, 'Emma Tammi', 'fnaf2_2.jpg', '2025-12-05', '17+'),
+(13, 'The Housemaid', '0', 'Horror / Thriller', 'Seorang pembantu rumah tangga menghadapi rahasia gelap keluarga majikannya. ‒ rilis 31 Desember 2025 ', 95, 'Jean Kim', 'the_housemaid.jpg', '2025-12-31', '17+');
+
 
 -- Tabel Jadwal
 CREATE TABLE jadwal (
@@ -69,6 +87,16 @@ CREATE TABLE reviews (
     Id_movie INT NOT NULL
 );
 
+CREATE TABLE `transaksi` (
+  `id_transaksi` int NOT NULL,
+  `id_jadwal` int NOT NULL,
+  `seat` varchar(5) NOT NULL,
+  `nama_customer` varchar(100) DEFAULT NULL,
+  `tanggal_beli` datetime DEFAULT CURRENT_TIMESTAMP,
+  `kursi` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
 -- ngehubungin antar table fk (foreign key)
 ALTER TABLE jadwal ADD CONSTRAINT fk_jadwal_film 
     FOREIGN KEY (Id_movie) REFERENCES movies(Id_movie) ON DELETE CASCADE;
@@ -90,6 +118,14 @@ ALTER TABLE reviews ADD CONSTRAINT fk_reviews_user
 
 ALTER TABLE reviews ADD CONSTRAINT fk_reviews_movie
     FOREIGN KEY (Id_movie) REFERENCES movies(Id_movie) ON DELETE CASCADE;
+
+ALTER TABLE movies
+    ADD COLUMN rating VARCHAR(10),
+    ADD COLUMN genre VARCHAR(100),
+    ADD COLUMN age_rating VARCHAR(5),
+    ADD COLUMN director VARCHAR(100);
+
+ALTER TABLE studios MODIFY capacity INT NOT NULL DEFAULT 0;
 
 
 -- nambah admin
